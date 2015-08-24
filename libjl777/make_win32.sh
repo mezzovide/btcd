2c85869988a4640f95b935133d4ea7fbd0e6cc26
jl777 <jameslee777@yahoo.com>
2015-08-24 09:54:53 -1100

    Revert "test"
    This reverts commit 41f52353833a9d3abc5820503bc4d31e2151f1b8.

--- libjl777/make_win32.sh
+++ libjl777/make_win32.sh
@@ -4,6 +4,7 @@ cp -n mxepatch/nanomsg.mk mxe/src/nanomsg.mk
 cp -n mxepatch/nanomsg-1.patch mxe/src/nanomsg-1.patch
 cp -n mxepatch/nanomsg-2.patch mxe/src/nanomsg-2.patch
 cp -n mxepatch/nanomsg-3-global.patch mxe/src/nanomsg-3-global.patch
+cp -n mxepatch/nanomsg-4-options.patch mxe/src/nanomsg-4-options.patch
 cd mxe
 patch -N -s --reject-file=- < ../mxepatch/mxe.patch
 cd src
@@ -40,19 +41,19 @@ export PATH=$PWD/usr/bin:$PATH
 cp ./usr/i686-w64-mingw32.static/include/winioctl.h ./usr/i686-w64-mingw32.static/include/WinIoCtl.h
 cp ./usr/i686-w64-mingw32.static/include/windows.h ./usr/i686-w64-mingw32.static/include/Windows.h
 cd ..
-cd nanomsg
-echo ">>>>>>>>>>>>>>>>>>building nanomsg"
-make clean
-sh ./autogen.sh
-CC=i686-w64-mingw32.static-gcc CXX=i686-w64-mingw32.static-g++ ./configure --disable-replication --enable-cxx --host i686-w64-mingw32.static
-make CFLAGS='-g -O2 -w -DNN_HAVE_WINDOWS -DNN_HAVE_MINGW -D_WIN32_WINNT=0x0600'
-cp .libs/libnanomsg.a ../libs/libnanomsg-win32.a
-cd ../
+#cd nanomsg
+#echo ">>>>>>>>>>>>>>>>>>building nanomsg"
+#make clean
+#sh ./autogen.sh
+#CC=i686-w64-mingw32.static-gcc CXX=i686-w64-mingw32.static-g++ ./configure --disable-replication --enable-cxx --host i686-w64-mingw32.static
+#make CFLAGS='-g -O2 -w -DNN_HAVE_WINDOWS -DNN_HAVE_MINGW -D_WIN32_WINNT=0x0600'
+#cp .libs/libnanomsg.a ../libs/libnanomsg-win32.a
+#cd ../
 rm mxe/usr/i686-w64-mingw32.static/include/objidl.h
 cp mxepatch/winheaders/objidl.h mxe/usr/i686-w64-mingw32.static/include/objidl.h
 cd miniupnpc
 sh make_win.sh
-echo ">>>>>>>>>>>>>>>>>>building mman-win32"
+#echo ">>>>>>>>>>>>>>>>>>building mman-win32"
 #cd ../mman-win32
 #make clean
 #./configure --cc=i686-w64-mingw32.static-gcc --enable-static
